aa5f4d1b5379051f8a08595019d5cd6e1354e3f7
BrunoGG69 <prathameshprabhakar27@gmail.com>
2025-08-05 12:57:30 +0530

    Updated UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,26 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        moveGradient: "moveGradient 10s ease infinite",
+        pulseBlur: "pulseBlur 6s ease-in-out infinite",
+      },
+      keyframes: {
+        moveGradient: {
+          "0%, 100%": { backgroundPosition: "0% 0%" },
+          "50%": { backgroundPosition: "100% 100%" },
+        },
+        pulseBlur: {
+          "0%, 100%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.1)" },
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
